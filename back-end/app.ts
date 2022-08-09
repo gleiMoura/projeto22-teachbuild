f6834c2a1c9e5e8a054fcf5c3b1ab5bd89cd3dc4
Gleison Moura <gle.bar.mou@gmail.com>
2022-08-09 20:10:32 -0300

    create folders to use layered architecture

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,10 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
+import router from "./src/routes";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(router);
 
 export default app;
