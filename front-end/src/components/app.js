401ad3399b3b7ccac29f372dde7057d163116ffe
Gleison Moura <gle.bar.mou@gmail.com>
2022-08-11 11:26:56 -0300

    create front-end folder

---
+++ front-end/src/components/app.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+export default function App() {
+    return (
+     <>
+        aqui está uma aplicação
+     </>
+    );
+}
+
+{/* <BrowserRouter>
+<Routes>
+    <Route path="/" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/timeline" element={<Timeline />} />
+    <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
+    <Route path="/users/:id" element={<UserPage />} />
+</Routes>
+</BrowserRouter> */}
