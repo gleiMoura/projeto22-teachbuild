0f854ada01326b1cc19fab3a69eb3d81076d0513
Gleison Moura <gle.bar.mou@gmail.com>
2022-08-18 01:10:44 -0300

    add wallet and like components

--- front-end/src/pages/teacherPage.js
+++ front-end/src/pages/teacherPage.js
@@ -6,7 +6,6 @@ import API from "../repository/API";
 
 export default function TeacherPage() {
     const userData = JSON.parse(localStorage.getItem("data"));
-    console.log(userData)
     const navigate = useNavigate()
     const days = ["seg", "ter", "qua", "qui", "sex", "sab", "dom"];
     const hours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
